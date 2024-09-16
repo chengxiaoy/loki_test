a513f65523208d2c374ad79a3a837e987052377b
chengying <chengying@choirai.com>
2024-09-16 18:57:42 +0800

    add multiline stage to pipeline

--- print_logfile.py
+++ print_logfile.py
@@ -3,7 +3,6 @@ import time
 import random
 from log_utils import MakeFileHandler
 
-
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
@@ -19,11 +18,24 @@ logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
 
 
+def raise_exception_method():
+    try:
+        1 / 0
+    except Exception as e:
+        raise ValueError("zero can't be divided") from e
+
+
 def log():
     logger.debug('This message should go to the log file')
+    logger.debug('This message should go to \n the log file')
     logger.info('So should this')
+    logger.info('So \n should \n this')
     logger.warning('And this, too')
     logger.error('And non-ASCII stuff, too, like Øresund and Malmö')
+    try:
+        raise_exception_method()
+    except Exception as e:
+        logger.exception(e)
 
 
 if __name__ == '__main__':
